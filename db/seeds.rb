69a5bade2734ec1e8cba2603b4c2423f7a0be048
Ceejayski <sirchijioke1@gmail.com>
2021-11-23 14:48:50 +0100

    Setup cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'admin', admin: true, email: 'admin', password: 'admin', password_confirmation: 'admin')
