4c4fb8be0d0f4a20c70471f28ca0a2d56b8c1eac
Ceejayski <sirchijioke1@gmail.com>
2021-11-25 17:36:44 +0100

    Allow patch and destroy actions in development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
         :headers => :any,
-        :methods => [:get, :post, :delete, :put, :options]
+        :methods => [:get, :post, :delete, :put, :options, :destroy, :patch]
   end
 end
