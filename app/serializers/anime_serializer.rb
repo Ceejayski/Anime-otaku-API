a8031b3fcee4197d96c429b8c63e68ab6650d4a4
Ceejayski <sirchijioke1@gmail.com>
2021-11-27 09:38:47 +0100

    Add slug to anime Json response

--- app/serializers/anime_serializer.rb
+++ app/serializers/anime_serializer.rb
@@ -1,6 +1,6 @@
 class AnimeSerializer
   include JSONAPI::Serializer
-  attributes :name, :description, :rating, :header_image, :genre_list
+  attributes :name, :description, :rating, :header_image, :genre_list, :slug
   attributes :related_animes do |object|
     object.find_related_genres.map { |x| x.slice(:name, :description, :rating, :genre_list, :header_image) }
       .sort_by { |hsh| hsh[:rating] }.reverse
